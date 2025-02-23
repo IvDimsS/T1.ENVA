781281010e010e245ee0f12b2e35da139f7c698c
dmitry <ivdimss@gmail.com>
2025-02-23 20:47:33 +0300

    test5

--- src/test/java/screens/ContactsPage.java
+++ src/test/java/screens/ContactsPage.java
@@ -6,7 +6,7 @@ public class ContactsPage extends BaseObject {
     String servicesTab = "//android.widget.FrameLayout[@content-desc=\"Лента\"]/android.view.ViewGroup/android.widget.TextView";
     String eventServicesTab = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.TextView";
     String eventWidget = "";
-    String eventWidgetCart = "";
+    String eventWidgetCart = "nn";
     String eventCalendarButton = "";
     String eventNotificationButton = "";
     String eventConnectButton = "";
